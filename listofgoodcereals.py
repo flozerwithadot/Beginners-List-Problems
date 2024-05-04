e733cee08a21247f3a5c9a6d8393c901861049dd
flozer :3 <161006821+flozerwithadot@users.noreply.github.com>
2024-05-05 11:54:37 +1200

    Create listofgoodcereals.py

---
+++ listofgoodcereals.py
@@ -0,0 +1,11 @@
+cerealList = []
+while True:
+    cereal = input("Enter a cereal: ")
+    if cereal == "weetbix":
+        print(cerealList)
+        break
+    elif cereal == "sultana and bran":
+        print(cerealList)
+        break
+    else:
+        cerealList.append(cereal)
